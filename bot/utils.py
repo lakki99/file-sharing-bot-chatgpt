102cb06ae2694e895baa5968f7a89a615ff9882e
lakki99 <pulka143341@gmail.com>
2025-05-25 08:54:48 +0530

    Update utils.py

--- bot/utils.py
+++ bot/utils.py
@@ -1,1 +1,16 @@
+# bot/utils.py
 
+import os
+from pyrogram import Client
+from pyrogram.types import Message
+
+from dotenv import load_dotenv
+load_dotenv("config.env")
+
+LOGS_CHANNEL_ID = int(os.getenv("LOGS_CHANNEL_ID"))
+
+async def log_to_channel(client: Client, text: str):
+    try:
+        await client.send_message(chat_id=LOGS_CHANNEL_ID, text=text)
+    except Exception as e:
+        print(f"Logging failed: {e}")
