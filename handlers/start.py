f3abf55852356e385bf7e826614d1bb72baeec12
lakki99 <pulka143341@gmail.com>
2025-05-27 07:10:24 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,3 +1,6 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
 def start_handlers(app):
     @app.on_message(filters.command("start") & filters.private)
     async def start(client, message: Message):
