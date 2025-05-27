a56ad442bc462a5261884cab05f103d8a56e38ca
lakki99 <pulka143341@gmail.com>
2025-05-27 05:48:26 +0530

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,14 @@
+from .start import start_handler
+from .verify import verify_handler
+from .link import link_handler
+from .batch import batch_handler
+from .stats import stats_handler
+from .admin import admin_handlers
+
+def register_handlers(app):
+    start_handler(app)
+    verify_handler(app)
+    link_handler(app)
+    batch_handler(app)
+    stats_handler(app)
+    admin_handlers(app)
